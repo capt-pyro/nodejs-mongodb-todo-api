8aabc1681a8e35f1d123398323a2cd17de45cb0c
capt-pyro <joe.nsj@gmail.com>
2017-06-14 18:31:44 +0100

    Added GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 var{mongoose} = require('./db/mongoose');
 var{Todo} = require('./models/todo');
 var{User} = require('./models/user');
-
+var {ObjectID} = require('mongodb');
 var app = express(); //startt express
 
 app.use(bodyParser.json());
@@ -27,6 +27,18 @@ app.get('/todos', (req,res) => {
 },(err) =>{
     res.status(400).send(err);
   });
+});
+
+//GET/todos/123456
+app.get('/todos/:id', (req,res) => {
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)) return res.status(404).send();
+    Todo.findById(id).then((todo) => {
+      if(!todo) return res.status(404).send();
+      res.send({todo});
+    }).catch((err) => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3000, () =>{
