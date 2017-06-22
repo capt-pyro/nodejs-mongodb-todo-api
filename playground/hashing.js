0dc7135930165a39f9f10bb28e475a6d4567b0d6
capt-pyro <joe.nsj@gmail.com>
2017-06-22 09:24:26 +0400

    hashed user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+ var password = '123abc';
+//
+// bcrypt.genSalt(10, (err,salt) => {
+//   bcrypt.hash(password,salt,(err, hash) => {
+//     console.log(hash);
+//   })
+// });
+
+var hashedpassword = '$2a$10$1U2Pz8OC2ix.t00Odjw0tuNWkMrq3N.GUo0dG3Wu.bhQpZ.8.WDR2';
+bcrypt.compare(password,hashedpassword, (err,res) => {
+  console.log(res);
+});
 // var message = "I'll be encrytped";
 // var hash = SHA256(message).toString();
 // console.log(hash);
 
 //jwt.sign hashes data
 //jwt.verify verfies it
-var data = {
-  id: 5
-};
-var token = jwt.sign(data,'123abc');
-console.log(token);
-var decoded = jwt.verify(token,'123abc');
-console.log('----------------------------------');
-console.log(decoded);
+// var data = {
+//   id: 5
+// };
+// var token = jwt.sign(data,'123abc');
+// console.log(token);
+// var decoded = jwt.verify(token,'123abc');
+// console.log('----------------------------------');
+// console.log(decoded);
