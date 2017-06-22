3daca9fb8afffaadaa14ec649e60d0d22c6e911e
capt-pyro <joe.nsj@gmail.com>
2017-06-22 08:21:16 +0400

    added GET/users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const _ = require('lodash');
 
+var{authenticate} = require('./middleware/authenticate');
 var{mongoose} = require('./db/mongoose');
 var{Todo} = require('./models/todo');
 var{User} = require('./models/user');
@@ -90,7 +91,9 @@ app.post('/users', (req,res) => {
 });
 
 
-
+app.get('/users/me', authenticate, (req,res) => {
+  res.send(req.user);
+});
 //connect to port 3000
 app.listen(port, () =>{
   console.log(`Started on port ${port}`);
