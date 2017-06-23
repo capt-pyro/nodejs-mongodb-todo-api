9ce3f239b7c9741efbf39ac8fa6b6fd1cd2b07ce
capt-pyro <joe.nsj@gmail.com>
2017-06-23 16:24:30 +0400

    added testing for POST/users/login route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -208,7 +208,7 @@ describe('/POST/users', () => {
         expect(user).toExist();
         expect(user.password).toNotBe(use.password);
         done();
-      });
+      }).catch((err) => done(err));
     });
   });
 
@@ -234,5 +234,51 @@ describe('/POST/users', () => {
     .send(use)
     .expect(400)
     .end(done);
+  });
+});
+
+describe('/POST/users/login', () => {
+  it('should send back the user if a valid request is sent', (done) => {
+    var body = users[1];
+    request(app)
+    .post('/users/login')
+    .send(body)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.email).toBe(users[1].email);
+      expect(res.header['x-auth']).toExist();
+    })
+    .end((err,res) => {
+      if(err) return done(err);
+
+      User.findById(users[1]._id).then((user) => {
+        expect(user.tokens[0]).toInclude({
+          access:'auth',
+          token: res.headers['x-auth']
+        });
+        done();
+      }).catch((err) => done(err));
+    });
+  });
+
+  it('should deny login if credentials aren\'t valid', (done) => {
+    var user = {
+      email: users[1].email,
+      password: users[1].password + '1'
+    }
+    request(app)
+    .post('/users/login')
+    .send(user)
+    .expect(400)
+    .expect((res) => {
+      expect(res.headers['x-auth']).toNotExist();
+    })
+    .end((err,res) => {
+      if(err) return done(err);
+      User.findById(users[1]._id).then((user) => {
+        expect(user.tokens.length).toBe(0);
+        done();
+      }).catch((e) => done(e));
+    });
   });
 });
