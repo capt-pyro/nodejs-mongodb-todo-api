cd8010f8288f5a3d2c4d11148cad4eac2772ef78
capt-pyro <joe.nsj@gmail.com>
2017-06-25 10:18:23 +0400

    removed sensitive info from config.js into config.json(local)

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,11 @@
 var env = process.env.NODE_ENV || 'development';
 
-if(env === 'development'){//local development using postman
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-}
-else if (env === 'test') {//used for testing on server.test.js
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-}
-else if (env === 'atlas') {//production using mongodb atlas service
-  process.env.PORT = 3000;
-  //  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-  process.env.MONGODB_URI = "mongodb://JoeAdmin:aaGPQY5yJQtBAZdc@joecluster-1-shard-00-00-1auep.mongodb.net:27017,joecluster-1-shard-00-01-1auep.mongodb.net:27017,joecluster-1-shard-00-02-1auep.mongodb.net:27017/testdatabase?ssl=true&replicaSet=JoeCluster-1-shard-0&authSource=admin";
+
+
+if(env === 'development' ||env === 'test' ) {
+  var config = require('./config.json');
+  var envConfig = config[env];
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
